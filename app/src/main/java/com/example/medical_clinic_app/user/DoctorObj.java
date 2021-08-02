fe8de13c5ea72f44544a9066a63a8d3d7430d0c3
Tony-beeper <tyx1999120755@gmail.com>
2021-08-02 20:37:12 +0800

    Update DoctorObj.java

--- app/src/main/java/com/example/medical_clinic_app/user/DoctorObj.java
+++ app/src/main/java/com/example/medical_clinic_app/user/DoctorObj.java
@@ -1,22 +1,42 @@
 package com.example.medical_clinic_app.user;
 
+import com.example.medical_clinic_app.appointment.Appointment;
+import com.example.medical_clinic_app.services.ClinicDao;
+
+
 import java.util.List;
+import java.util.ArrayList;
 
 public class DoctorObj implements Doctor{
-    
+    String name;
+    String username;
+    String password;
+    ArrayList<Integer> appointments;
+
+
+
+    public DoctorObj(String name, String username, String password){
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
 
     @Override
     public List<Long> getAvailabilities(){
+
+
         return null;
     }
 
     @Override
     public List<Integer> getAppointments() {
-        return null;
+
+        return appointments;
     }
 
     @Override
     public void addAppointment(int appointmentId) {
-
+        appointments.add(appointmentId);
     }
 }
