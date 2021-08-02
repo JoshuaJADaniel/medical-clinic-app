49051cc4297403f38ce419b6e8325121151fe14e
Tony-beeper <tyx1999120755@gmail.com>
2021-08-02 20:09:45 +0800

    added DoctorClass

---
+++ app/src/main/java/com/example/medical_clinic_app/user/DoctorObj.java
@@ -0,0 +1,22 @@
+package com.example.medical_clinic_app.user;
+
+import java.util.List;
+
+public class DoctorObj implements Doctor{
+    
+
+    @Override
+    public List<Long> getAvailabilities(){
+        return null;
+    }
+
+    @Override
+    public List<Integer> getAppointments() {
+        return null;
+    }
+
+    @Override
+    public void addAppointment(int appointmentId) {
+
+    }
+}
