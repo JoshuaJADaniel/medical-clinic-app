816bcf6ea6861b4aedd14addf2eb20c9dd4201bc
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-02 10:14:18 -0400

    Add date conversion access to DAO to avoid passing redundant information to methods

--- app/src/main/java/com/example/medical_clinic_app/services/ClinicDao.java
+++ app/src/main/java/com/example/medical_clinic_app/services/ClinicDao.java
@@ -1,6 +1,7 @@
 package com.example.medical_clinic_app.services;
 
 import com.example.medical_clinic_app.appointment.Appointment;
+import com.example.medical_clinic_app.time.DateConverter;
 import com.example.medical_clinic_app.user.Doctor;
 import com.example.medical_clinic_app.user.Patient;
 
@@ -8,6 +9,7 @@ public interface ClinicDao {
     Patient findPatientByUsername(String username);
     Doctor findDoctorByUsername(String username);
     Appointment findAppointmentById(int id);
+    DateConverter getDateConverter();
 
     void addPatient(Patient patient);
     void addDoctor(Doctor doctor);
