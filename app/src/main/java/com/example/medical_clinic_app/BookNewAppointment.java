2827ab69df7899a52bdc7ae5f5faaf06c4c7a972
Ultrahi <sanjaymylanathan85@gmail.com>
2021-08-03 11:19:52 -0400

    UI layouts and files created
    Created the other 2 activities from the User Interface Mockups, the book new appointments one and the doctor availability one. However, note that I'm unsure of how to proceed with making the dropdown list work and link to database, so that is incomplete.

---
+++ app/src/main/java/com/example/medical_clinic_app/BookNewAppointment.java
@@ -0,0 +1,14 @@
+package com.example.medical_clinic_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookNewAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_new_appointment);
+    }
+}
