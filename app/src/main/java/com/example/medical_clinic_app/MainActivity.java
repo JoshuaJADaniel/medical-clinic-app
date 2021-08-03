272ebbf00c99f1cbd4112d7524155942cfd65269
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-03 10:10:09 -0400

    Rename view files to more generic names

--- app/src/main/java/com/example/medical_clinic_app/MainActivity.java
+++ app/src/main/java/com/example/medical_clinic_app/MainActivity.java
@@ -5,33 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-
-import com.example.medical_clinic_app.services.ClinicFirebaseDao;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
-    FirebaseDatabase rootNode;
-    DatabaseReference reference;
-
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final String KEY_USER = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ClinicFirebaseDao clinic = new ClinicFirebaseDao();
-
         setContentView(R.layout.activity_main);
     }
 
     public void bookAppointment(View view) {
-        Intent intent = new Intent(this, DisplayDoctors.class);
-        rootNode = FirebaseDatabase.getInstance();
-        reference = rootNode.getReference("doctors");
-
-
+        Intent intent = new Intent(this, PatientDashboard.class);
         startActivity(intent);
     }
 }
