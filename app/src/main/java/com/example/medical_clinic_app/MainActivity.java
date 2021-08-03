9e7982bca35d1315a1004d6f4f521dd06f4a17e1
Tony-beeper <tyx1999120755@gmail.com>
2021-08-03 20:15:52 +0800

    Main Activity(Patient Page)
    Still need to implement the recycler view for dropdown list of upcoming appointments
    Also I'm trying to figure out how to work with the Firebase to extract data+Display

--- app/src/main/java/com/example/medical_clinic_app/MainActivity.java
+++ app/src/main/java/com/example/medical_clinic_app/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.medical_clinic_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.medical_clinic_app.services.ClinicFirebaseDao;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ClinicFirebaseDao clinic = new ClinicFirebaseDao();
+
         setContentView(R.layout.activity_main);
+    }
+
+    public void bookAppointment(View view) {
+        Intent intent = new Intent(this, DisplayDoctors.class);
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("doctors");
+
+
+        startActivity(intent);
     }
 }
