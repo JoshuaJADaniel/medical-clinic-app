533e4e45639010bc0fd8caf5f4085f20c8a89df2
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-02 20:48:40 -0400

    Update interface and implementations of DAO due to Firebase disallowing synchronous returns

--- app/src/main/java/com/example/medical_clinic_app/services/ClinicDao.java
+++ app/src/main/java/com/example/medical_clinic_app/services/ClinicDao.java
@@ -1,20 +1,9 @@
 package com.example.medical_clinic_app.services;
 
-import com.example.medical_clinic_app.appointment.Appointment;
-import com.example.medical_clinic_app.user.Doctor;
-import com.example.medical_clinic_app.user.Patient;
-
-import java.time.LocalDateTime;
-import java.util.List;
+import com.google.firebase.database.DatabaseReference;
 
 public interface ClinicDao {
-    Patient findPatientByUsername(String username);
-    Doctor findDoctorByUsername(String username);
-    Appointment findAppointmentById(int id);
-
-    void addPatient(Patient patient);
-    void addDoctor(Doctor doctor);
-    void addAppointment(Appointment appointment);
-
-    List<LocalDateTime> getAvailabilities(Doctor doctor, LocalDateTime date);
+    DatabaseReference getDoctorsRef();
+    DatabaseReference getPatientsRef();
+    DatabaseReference getAppointmentsRef();
 }
---
+++ app/src/main/java/com/example/medical_clinic_app/services/ClinicFirebaseDao.java
@@ -0,0 +1,26 @@
+package com.example.medical_clinic_app.services;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ClinicFirebaseDao implements ClinicDao {
+    private static final FirebaseDatabase db = FirebaseDatabase.getInstance();
+    private static final DatabaseReference doctorsRef = db.getReference("doctors");
+    private static final DatabaseReference patientsRef = db.getReference("patients");
+    private static final DatabaseReference appointmentsRef = db.getReference("appointments");
+
+    @Override
+    public DatabaseReference getDoctorsRef() {
+        return doctorsRef;
+    }
+
+    @Override
+    public DatabaseReference getPatientsRef() {
+        return patientsRef;
+    }
+
+    @Override
+    public DatabaseReference getAppointmentsRef() {
+        return appointmentsRef;
+    }
+}
