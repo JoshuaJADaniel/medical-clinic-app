50d59f7a10a62ffd16ce3fe29724a5f47ec6f360
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-03 00:36:12 -0400

    Change access modifiers to be more restrictive and add setters as part of interface

--- app/src/main/java/com/example/medical_clinic_app/appointment/Appointment.java
+++ app/src/main/java/com/example/medical_clinic_app/appointment/Appointment.java
@@ -1,7 +1,12 @@
 package com.example.medical_clinic_app.appointment;
 
 public interface Appointment {
-    long getTime();
+    long getDate();
+    void setDate(long date);
+
     String getDoctor();
+    void setDoctor(String doctor);
+
     String getPatient();
+    void setPatient(String patient);
 }
--- app/src/main/java/com/example/medical_clinic_app/appointment/GeneralAppointment.java
+++ app/src/main/java/com/example/medical_clinic_app/appointment/GeneralAppointment.java
@@ -1,23 +1,28 @@
 package com.example.medical_clinic_app.appointment;
 
-public class GeneralAppointment implements Appointment{
+public class GeneralAppointment implements Appointment {
+    private long date;
+    private String patient;
+    private String doctor;
 
-    long date;
-    String patient;
-    String doctor;
+    public void Appointment() {
 
-    public void Appointment(){
     }
 
-    public void Appointment(long date, String patient, String doctor){
-
+    public void Appointment(long date, String patient, String doctor) {
         this.date = date;
         this.patient = patient;
         this.doctor = doctor;
     }
+
     @Override
-    public long getTime() {
+    public long getDate() {
         return date;
+    }
+
+    @Override
+    public void setDate(long date) {
+        this.date = date;
     }
 
     @Override
@@ -26,22 +31,17 @@ public class GeneralAppointment implements Appointment{
     }
 
     @Override
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+
+    @Override
     public String getPatient() {
         return patient;
     }
 
-    public void setDate(long date){
-
-        this.date = date;
-    }
-
-    public void setPatient(String patient){
-
+    @Override
+    public void setPatient(String patient) {
         this.patient = patient;
-    }
-
-    public void setDoctor(String doctor){
-
-        this.doctor = doctor;
     }
 }
