74951954a5c560cc8f61a95a4ada4765456cdd33
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-04 21:54:50 -0400

    Remove age attribute since date of birth is sufficient

--- app/src/main/java/com/example/medical_clinic_app/user/PatientObj.java
+++ app/src/main/java/com/example/medical_clinic_app/user/PatientObj.java
@@ -3,7 +3,6 @@ package com.example.medical_clinic_app.user;
 import java.util.List;
 
 public class PatientObj implements Patient {
-    private int age;
     private String name;
     private String gender;
     private String username;
@@ -15,14 +14,12 @@ public class PatientObj implements Patient {
 
     }
 
-    public PatientObj(int age,
-                      String name,
+    public PatientObj(String name,
                       String gender,
                       String username,
                       String password,
                       long dateOfBirth,
                       List<Integer> appointments) {
-        this.age = age;
         this.name = name;
         this.gender = gender;
         this.username = username;
@@ -94,7 +91,6 @@ public class PatientObj implements Patient {
     @Override
     public String toString() {
         return "PatientObj{" +
-                "age=" + age +
                 ", name='" + name + '\'' +
                 ", gender='" + gender + '\'' +
                 ", username='" + username + '\'' +
