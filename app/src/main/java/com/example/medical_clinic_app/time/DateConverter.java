9636bebdcf4c475ec05be8951f57963d4d2f5866
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-07 17:18:22 -0400

    Add more utility methods to easily format dates in a consistent manner

--- app/src/main/java/com/example/medical_clinic_app/time/DateConverter.java
+++ app/src/main/java/com/example/medical_clinic_app/time/DateConverter.java
@@ -5,4 +5,12 @@ import java.time.LocalDateTime;
 public interface DateConverter {
     LocalDateTime longToDate(long time);
     long dateToLong(LocalDateTime time);
+
+    String getFormattedDate(long time);
+    String getFormattedDate(LocalDateTime time);
+
+    String getFormattedTime(long time);
+    String getFormattedTime(LocalDateTime time);
+
+    String getLocale();
 }
--- app/src/main/java/com/example/medical_clinic_app/time/UtcDateConverter.java
+++ app/src/main/java/com/example/medical_clinic_app/time/UtcDateConverter.java
@@ -1,5 +1,7 @@
 package com.example.medical_clinic_app.time;
 
+import android.annotation.SuppressLint;
+
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
@@ -12,5 +14,32 @@ public class UtcDateConverter implements DateConverter {
     @Override
     public long dateToLong(LocalDateTime time) {
         return time.toEpochSecond(ZoneOffset.UTC);
+    }
+
+    @Override
+    public String getFormattedDate(long time) {
+        return getFormattedDate(longToDate(time));
+    }
+
+    @Override
+    @SuppressLint("DefaultLocale")
+    public String getFormattedDate(LocalDateTime time) {
+        return String.format("%s %d, %d", time.getMonth(), time.getDayOfMonth(), time.getYear());
+    }
+
+    @Override
+    public String getFormattedTime(long time) {
+        return getFormattedTime(longToDate(time));
+    }
+
+    @Override
+    @SuppressLint("DefaultLocale")
+    public String getFormattedTime(LocalDateTime time) {
+        return String.format("%02d:%02d", time.getHour(), time.getMinute());
+    }
+
+    @Override
+    public String getLocale() {
+        return "UTC";
     }
 }
