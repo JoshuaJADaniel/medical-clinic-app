6e52b69c8cb24a8f3aa78bb149bf53b5877a06cb
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-07 17:42:37 -0400

    Add more toast errors to be used across application (database + forms)

--- app/src/main/java/com/example/medical_clinic_app/utils/ErrorToasts.java
+++ app/src/main/java/com/example/medical_clinic_app/utils/ErrorToasts.java
@@ -4,7 +4,31 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class ErrorToasts {
-    public static void databaseToast(Context context) {
-        Toast.makeText(context, "Something went wrong accessing the database! Please check your internet connection or try again later", Toast.LENGTH_LONG).show();
+    public static void databaseDoctorError(Context context) {
+        databaseError(context, "Could not retrieve doctor from database");
+    }
+
+    public static void databasePatientError(Context context) {
+        databaseError(context, "Could not retrieve patient from database");
+    }
+
+    public static void databaseAppointmentError(Context context) {
+        databaseError(context, "Could not retrieve appointment from database");
+    }
+
+    private static void databaseError(Context context, String message) {
+        Toast.makeText(context, String.format("%s! Please check your internet connection or try again later.", message), Toast.LENGTH_LONG).show();
+    }
+
+    public static void usernamePasswordError(Context context) {
+        Toast.makeText(context, "Username or password is incorrect!", Toast.LENGTH_LONG).show();
+    }
+
+    public static void emptyFieldsError(Context context) {
+        Toast.makeText(context, "One or more fields are empty!", Toast.LENGTH_LONG).show();
+    }
+
+    public static void usernameTaken(Context context) {
+        Toast.makeText(context, "Sorry, that username is already taken!", Toast.LENGTH_LONG).show();
     }
 }
