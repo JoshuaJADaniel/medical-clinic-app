8bd097c971226e8d2f9ab61df9797bd54e188706
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-07 21:41:38 -0400

    Fixed bugs associated with filtering (reverting to unfiltered view had issues) and removed dependency on strings.xml to reduce coupling

--- app/src/main/java/com/example/medical_clinic_app/AvailableDoctorListActivity.java
+++ app/src/main/java/com/example/medical_clinic_app/AvailableDoctorListActivity.java
@@ -1,6 +1,7 @@
 package com.example.medical_clinic_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -22,139 +23,144 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import com.example.medical_clinic_app.services.ClinicDao;
 import com.example.medical_clinic_app.services.ClinicFirebaseDao;
+import com.example.medical_clinic_app.user.DoctorSpecializations;
+import com.example.medical_clinic_app.user.UserGenders;
+import com.example.medical_clinic_app.utils.ErrorToasts;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
-public class AvailableDoctorListActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
-    private List<Doctor> DoctorList;
-    private Doctor doctor;
+public class AvailableDoctorListActivity extends AppCompatActivity {
+    private List<Doctor> doctorList;
+    private List<Doctor> filteredDoctorList;
 
-    private RecyclerView recyclerDoctorsList;
-    private AdapterRecyclerDoctorsAvailable adapterDoctorsList;
-    private String GenderFlag = "Gender";
-    public String Gender = "Gender";
-    private String Specialization = "Monkey";
+    private RecyclerView recyclerDoctorList;
+    private AdapterRecyclerDoctorsAvailable adapterDoctorList;
 
-    private List<String> GenderList;
-    private List<String> SpecializationList;
+    private String currentGender;
+    private String currentSpecialization;
+    private final String genderPrompt = "Select Gender";
+    private final String specializationPrompt = "Select Specialization";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_available_doctor_list);
-        setUpGenderList();
-        setUpSpecializationList();
 
+        recyclerDoctorList = findViewById(R.id.recyclerDoctorList);
 
-        DoctorList = new ArrayList<>();
+        // Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbarAvailableDoctors);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_chevron);
 
-
-        recyclerDoctorsList = findViewById(R.id.recyclerDoctorList);
-
+        // Create adapter
+        doctorList = new ArrayList<>();
+        filteredDoctorList = new ArrayList<>();
         setDoctorAdapter();
-        // the Spinner will automatically trigger the setDoctorList at the beginning
-        //so no need to setDoctorList up
-        Spinner GenderSpinner = findViewById(R.id.GenderSpinner);
-        ArrayAdapter<CharSequence> GenderAdapter = ArrayAdapter.createFromResource(this,R.array.GenderList, android.R.layout.simple_spinner_item);
-        GenderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        GenderSpinner.setAdapter(GenderAdapter);
-        GenderSpinner.setOnItemSelectedListener(this);
+        setDoctorList();
 
+        // Initialize data for spinners
+        List<String> genders = new ArrayList<>(UserGenders.getGenders());
+        genders.add(0, genderPrompt);
 
-        Spinner SpecializationSpinner = findViewById(R.id.SpecializationSpinner);
-        ArrayAdapter<CharSequence> SpecializationAdapter = ArrayAdapter.createFromResource(this,R.array.SpecializationList, android.R.layout.simple_spinner_item);
-        SpecializationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        SpecializationSpinner.setAdapter(SpecializationAdapter);
-        SpecializationSpinner.setOnItemSelectedListener(this);
+        List<String> specializations = new ArrayList<>(DoctorSpecializations.getSpecializations());
+        specializations.add(0, specializationPrompt);
 
+        // Initialize spinners
+        Spinner spnGenders = findViewById(R.id.spnGenders);
+        ArrayAdapter<String> gendersAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, genders);
+        gendersAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnGenders.setAdapter(gendersAdapter);
+
+        Spinner spnSpecializations = findViewById(R.id.spnSpecializations);
+        ArrayAdapter<String> specializationsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, specializations);
+        specializationsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnSpecializations.setAdapter(specializationsAdapter);
+
+        // Add event listeners to spinners
+        spnGenders.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                currentGender = adapterView.getItemAtPosition(position).toString();
+                filterDoctorList();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+
+        spnSpecializations.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                currentSpecialization = adapterView.getItemAtPosition(position).toString();
+                filterDoctorList();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
     }
 
-    private void setUpSpecializationList() {
-
-        SpecializationList = new ArrayList<>();
-        SpecializationList.add("Specialization");
-        SpecializationList.add("Allergist");
-        SpecializationList.add("Cardiologist");
-        SpecializationList.add("Dentist");
-        SpecializationList.add("Dermatologist");
-        SpecializationList.add("Psychiatrist");
-        SpecializationList.add("Radiologist");
-        SpecializationList.add("Urologist");
-
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 
-    private void setUpGenderList() {
-        GenderList = new ArrayList<>();
+    public void setDoctorAdapter() {
+        adapterDoctorList = new AdapterRecyclerDoctorsAvailable(filteredDoctorList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerDoctorList.setItemAnimator(new DefaultItemAnimator());
+        recyclerDoctorList.setLayoutManager(layoutManager);
+        recyclerDoctorList.setAdapter(adapterDoctorList);
     }
 
-    public void setDoctorAdapter(){
-        adapterDoctorsList = new AdapterRecyclerDoctorsAvailable(DoctorList);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerDoctorsList.setLayoutManager(layoutManager);
-        recyclerDoctorsList.setItemAnimator(new DefaultItemAnimator());
-        recyclerDoctorsList.setAdapter(adapterDoctorsList);
-    }
-
-    public void setDoctorList(String Gender, String Specialization){
-        DoctorList.removeAll(DoctorList);
-
-        ClinicDao dao = new ClinicFirebaseDao();
-        DatabaseReference DoctorDatabase = dao.getDoctorsRef();
-        DoctorDatabase.addValueEventListener(new ValueEventListener() {
+    public void setDoctorList() {
+        new ClinicFirebaseDao().getDoctorsRef().addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for(DataSnapshot datasnapshot :snapshot.getChildren()) {
-
+                for (DataSnapshot datasnapshot : snapshot.getChildren()) {
                     Doctor doctor = datasnapshot.getValue(DoctorObj.class);
-                    if (Gender.equals("Gender") && Specialization.equals("Specialization"))
-                        DoctorList.add(doctor);
-                    else if (Gender.equals("Gender") && !(Specialization.equals("Specialization"))) {
-                        if (doctor.getSpecialization().equals(Specialization))
-                            DoctorList.add(doctor);
-                    } else if (!(Gender.equals("Gender")) && Specialization.equals("Specialization")) {
-                        if (doctor.getGender().equals(Gender)) DoctorList.add(doctor);
-                    } else if (doctor.getGender().equals(Gender) && doctor.getSpecialization().equals(Specialization)) DoctorList.add(doctor);
+
+                    if (doctor == null) {
+                        ErrorToasts.databaseDoctorError(AvailableDoctorListActivity.this);
+                        return;
+                    }
+
+                    if (doctor.getIsActive()) {
+                        doctorList.add(doctor);
+                        filteredDoctorList.add(doctor);
+                        adapterDoctorList.notifyItemInserted(filteredDoctorList.size() - 1);
+                    }
                 }
-
-                adapterDoctorsList.notifyDataSetChanged();
-
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Log.e(null, error.toString());
-
+                ErrorToasts.databaseDoctorError(AvailableDoctorListActivity.this);
             }
         });
 
-
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        String text = adapterView.getItemAtPosition(i).toString().trim();
-        if(SpecializationList.contains(text)){
-            Specialization = text;
-        }else {
-            Gender = text;
+    public void filterDoctorList() {
+        if (currentGender == null || currentSpecialization == null) return;
+
+        filteredDoctorList.clear();
+        for (Doctor doctor : doctorList) {
+            if ((currentGender.equals(genderPrompt) || currentGender.equals(doctor.getGender())) &&
+                    (currentSpecialization.equals(specializationPrompt) || currentSpecialization.equals(doctor.getSpecialization()))) {
+                filteredDoctorList.add(doctor);
+            }
         }
 
-
-        if(!(Gender.equals("Gender")) || !(Specialization.equals("Specialization"))) setDoctorList(Gender, Specialization);
-        else if( Gender.equals("Gender") && Specialization.equals("Specialization")) setDoctorList(Gender, Specialization);
-
-        Toast.makeText(adapterView.getContext(),text, Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> adapterView) {
-
+        adapterDoctorList.notifyDataSetChanged();
     }
 }
