aec82286c88edbe42f5c09bb1f812beda0807db1
Joshua Daniel <joshuajadaniel@gmail.com>
2021-08-09 01:00:24 -0400

    Add frontend text to inform users of empty doctor list (due to filters)

--- app/src/main/java/com/example/medical_clinic_app/AvailableDoctorListActivity.java
+++ app/src/main/java/com/example/medical_clinic_app/AvailableDoctorListActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.medical_clinic_app.adapters.AdapterRecyclerDoctorsAvailable;
@@ -23,6 +24,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.medical_clinic_app.services.ClinicFirebaseDao;
 import com.example.medical_clinic_app.user.DoctorSpecializations;
@@ -35,16 +37,25 @@ import com.google.firebase.database.ValueEventListener;
 public class AvailableDoctorListActivity extends AppCompatActivity {
     public final static String KEY_PATIENT = "KEY_PATIENT";
 
-    private List<Doctor> doctorList;
-    private List<Doctor> filteredDoctorList;
+    private final List<Doctor> doctorList = new ArrayList<>();
+    private final List<Doctor> filteredDoctorList = new ArrayList<>();
+
+    private Toolbar toolbar;
 
     private RecyclerView recyclerDoctorList;
     private AdapterRecyclerDoctorsAvailable adapterDoctorList;
+
+    private TextView txtRecyclerPrompt;
+
+    private Spinner spnGenders;
+    private Spinner spnSpecializations;
 
     private String currentGender;
     private String currentSpecialization;
     private final String genderPrompt = "Select Gender";
     private final String specializationPrompt = "Select Specialization";
+    private final String noDoctorsPrompt = "No doctors found! Consider changing your filters.";
+    private final String doctorsFoundPrompt = "Select a doctor below to book an appointment";
 
     private String patientUsername;
 
@@ -53,40 +64,50 @@ public class AvailableDoctorListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_available_doctor_list);
 
-        recyclerDoctorList = findViewById(R.id.recyclerDoctorList);
-        patientUsername = getIntent().getStringExtra(KEY_PATIENT);
+        Intent intent = getIntent();
+        patientUsername = intent.getStringExtra(KEY_PATIENT);
 
-        // Toolbar
-        Toolbar toolbar = findViewById(R.id.toolbarAvailableDoctors);
+        toolbar = findViewById(R.id.toolbarAvailableDoctors);
+
+        spnGenders = findViewById(R.id.spnGenders);
+        spnSpecializations = findViewById(R.id.spnSpecializations);
+        recyclerDoctorList = findViewById(R.id.recyclerDoctorList);
+        txtRecyclerPrompt = findViewById(R.id.txtRecyclerPrompt);
+        txtRecyclerPrompt.setText(noDoctorsPrompt);
+
+        initializeToolbar();
+        initializeSpinners();
+        setDoctorAdapter();
+        setDoctorList();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    private void initializeToolbar() {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_chevron);
+    }
 
-        // Create adapter
-        doctorList = new ArrayList<>();
-        filteredDoctorList = new ArrayList<>();
-        setDoctorAdapter();
-        setDoctorList();
-
-        // Initialize data for spinners
+    private void initializeSpinners() {
         List<String> genders = new ArrayList<>(UserGenders.getGenders());
-        genders.add(0, genderPrompt);
-
         List<String> specializations = new ArrayList<>(DoctorSpecializations.getSpecializations());
+
+        genders.add(0, genderPrompt);
         specializations.add(0, specializationPrompt);
 
-        // Initialize spinners
-        Spinner spnGenders = findViewById(R.id.spnGenders);
         ArrayAdapter<String> gendersAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, genders);
         gendersAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spnGenders.setAdapter(gendersAdapter);
 
-        Spinner spnSpecializations = findViewById(R.id.spnSpecializations);
         ArrayAdapter<String> specializationsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, specializations);
         specializationsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spnSpecializations.setAdapter(specializationsAdapter);
 
-        // Add event listeners to spinners
         spnGenders.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
@@ -110,12 +131,6 @@ public class AvailableDoctorListActivity extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
     }
 
     public void setDoctorAdapter() {
@@ -167,5 +182,6 @@ public class AvailableDoctorListActivity extends AppCompatActivity {
         }
 
         adapterDoctorList.notifyDataSetChanged();
+        txtRecyclerPrompt.setText(filteredDoctorList.size() == 0 ? noDoctorsPrompt : doctorsFoundPrompt);
     }
 }
